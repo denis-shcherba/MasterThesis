93d747c6e1f6c8cc4e1baf8c1347e39bb9cb3e66
Denis Shcherba <d.e.shcherba@gmail.com>
2025-03-21 14:13:58 +0100

    now with random book (singular) generation in shelf

--- MasterThesis/premanips/pull_book.py
+++ MasterThesis/premanips/pull_book.py
@@ -4,17 +4,16 @@ import time
 import MasterThesis.manipulation as manip
 from MasterThesis.shelf import generate_shelf
 from MasterThesis.high_level_methods import RobotEnviroment
-from MasterThesis.book_spawning import generate_random_box_positions
+from MasterThesis.book_spawning import generate_random_box_params
 
 C = ry.Config()
 C.addFile(ry.raiPath('../rai-robotModels/scenarios/pandaSingle.g'))
 
-
 C.delFrame("panda_collCameraWrist")
-C.getFrame("table").setShape(ry.ST.ssBox, size=[1., 1., .1, .02])
+C.getFrame("table").setShape(ry.ST.ssBox, size=[.5, 1, .1, .005]).setColor(np.array([242, 240, 216])/255)   # Real size [1.1, 1.2, .02, .005]
 
 # Shelf
-pos = np.array([1., 0., .3])
+pos = np.array([.8, 0., .3])
 generate_shelf(C, pos, base_quaternion=[1, 0, 0, 1], openings_small=[4, 11], equidistant=False)
 
 # for convenience, a few definitions:
@@ -27,6 +26,8 @@ color = [1., 0., 0.]
 shelfBottomFrame = C.getFrame("big_xy_bottom_0_1")
 
 shelf_depth = shelfBottomFrame.getSize()[1]
+shelf_width = shelfBottomFrame.getSize()[0]
+shelf_height = shelfBottomFrame.getSize()[2]
 
 # Example usage
 shelf_size = (shelfBottomFrame.getSize()[0], shelfBottomFrame.getSize()[1], shelfBottomFrame.getSize()[2])  # Fixed shelf dimensions (X_s, Y_s, Z_s)
@@ -34,16 +35,19 @@ shelf_size = (shelfBottomFrame.getSize()[0], shelfBottomFrame.getSize()[1], shel
 box_size_ranges = {  # Variable box dimensions
     'x': (.1, .15),  # X_b range
     'y': (.14, .23),  # Y_b range
-    'z': (.007, .04),   # Z_b range
+    'z': (.009, .045),   # Z_b range
 }
 
-samples = generate_random_box_positions(shelf_size, box_size_ranges, num_samples=100)
+samples = generate_random_box_params(shelf_size, box_size_ranges, num_samples=10)
 
-for i in range(10):
-    print(samples[i])
+shelf_corner = np.array([
+    (shelfBottomFrame.getPosition()[:3] + np.array([-shelf_depth/2, -shelf_width/2, 0])),
+])
+
+for book_params in samples:
     C.addFrame(f"target_book") \
-        .setPosition([.64, .02, 1.18]) \
-        .setShape(ry.ST.ssBox, size=[samples[i][0], samples[i][1], samples[i][2], 0.01]) \
+        .setPosition(shelf_corner + np.append(book_params[3:5], (shelf_height+book_params[2])/2)) \
+        .setShape(ry.ST.ssBox, size=[book_params[0], book_params[1], book_params[2], 0.005]) \
         .setColor(np.random.rand(3)) \
         .setContact(1) \
         .setMass(.1)
@@ -52,18 +56,19 @@ for i in range(10):
     C.view(False)
 
 C.addFrame(f"target_book") \
-    .setPosition([.64, .02, 1.18]) \
-    .setShape(ry.ST.ssBox, size=[samples[-1][0], samples[-1][1], samples[-1][2], 0.01]) \
-    .setColor(np.random.rand(3)) \
+    .setPosition([ 0.4757264, -0.05907275, 1.16879413]) \
+    .setShape(ry.ST.ssBox, size=[0.14610244, 0.22199346, 0.01758827, 0.005]) \
+    .setColor([1, 0, 0]) \
     .setContact(1) \
     .setMass(.1)
     
-
+# target at the middle of the shelf ending
 target = np.array([
     (shelfBottomFrame.getPosition()[:2] + np.array([-shelf_depth/2, 0])),
 ])
 target = np.append(target, C.getFrame("target_book").getPosition()[2])
 
+C.addFrame("target").setShape(ry.ST.marker, .1).setPosition(target)
 C.view(True)
 
 
